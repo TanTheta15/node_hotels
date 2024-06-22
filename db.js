8d535379808d2c18188d1474be75f13a3ec204c5
Soumyojit De <soumyojitde.msd@gmail.com>
2024-06-22 19:50:59 +0530

    Latest commit for hotels 1

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 //Define the mongoDB connection URL
-const mongoURL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/hotels';     //you can replace /hotels name at the end with your own database name
+const mongoURL = process.env.MONGODB_URL;     //you can replace /hotels name at the end with your own database name
 
 //Set up MongoDB connection
 mongoose.connect(mongoURL, {
